b024071d70cea89e0d0a1d906b2c83ae6c05678f
Gabriela Rezende <gabriela.mrezende@outlook.com.br>
2022-08-03 21:39:16 -0300

    criação de rotas e de formulário

--- introducao-ao-angular-8/course-manager/src/app/courses/course.service.ts
+++ introducao-ao-angular-8/course-manager/src/app/courses/course.service.ts
@@ -13,6 +13,17 @@ export class CourseService {
         return COURSES;
     }
 
+    retrieveById(id: number): Course{
+        return COURSES.find((courseIterator: Course) => courseIterator.id === id);
+    }
+
+    save(course: Course): void{
+        if (course.id) {
+            const index = COURSES.findIndex((courseIterator: Course) => courseIterator.id === course.id);
+            COURSES[index] = course;
+        }
+    }
+
     // private coursesUrl: string = 'http://localhost:3100/api/courses';
 
     // constructor(private httpClient: HttpClient) { }
